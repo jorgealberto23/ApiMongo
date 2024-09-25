2b4389832945dbd97f869518938a3fed6934613b
jorgealberto23 <jorge.rocha5@etec.sp.gov.br>
2024-09-24 21:44:47 -0300

    update e delte

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.get('/', (reg, res) => {
 
 
 //Create
-app.post('/person', async (req, res)=>{
-    const {name, salary, approved} = req.body
+app.post('/person', async (req, res) => {
+    const { name, salary, approved } = req.body
 
     const person = {
         name,
@@ -29,24 +29,83 @@ app.post('/person', async (req, res)=>{
 
     try {
         await Person.create(person)
-        res.status(200).json({message: "Pessoa inserida no sistema"})
+        res.status(200).json({ message: "Pessoa inserida no sistema" })
     } catch (error) {
-        res.status(500).json({erro: error})       
+        res.status(500).json({ erro: error })
     }
 
 })
 
 //Read
-app.get("/person", async (req, res)=>{
+app.get("/person", async (req, res) => {
     try {
         const people = await Person.find()
-        res.status(200).json({people})
+        res.status(200).json({ people })
     } catch (error) {
-        res.status(500).json({erro: error})       
+        res.status(500).json({ erro: error })
     }
 })
-//UPDATE
-//DELETE
+
+//Read by id
+app.get("/person/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+        const people = await Person.findOne({ _id: id })
+
+        if (!people) {
+            res.status(422).json({ message: "Usuário não encontrado!" })
+            return
+        }
+
+        res.status(200).json(people)
+    } catch (error) {
+        res.status(500).json({ erro: error })
+    }
+})
+
+//Update
+app.patch("/person/:id", async (req, res) => {
+    const id = req.params.id
+
+    const {name, salary, approved} = req.body
+
+    const person = {
+        name,
+        salary,
+        approved
+    }
+
+    try {
+        const updatePerson = await Person.updateOne({ _id: id }, person)
+
+        if (updatePerson.matchedCount === 0) {
+            res.status(422).json({message: "Usuário não encontrado!"})
+            return
+        }
+        res.status(200).json(person)
+    } catch (error) {
+        res.status(500).json({ erro: error })
+    }
+})
+
+//Delete
+app.delete("/person/:id", async (req, res) => {
+    const id = req.params.id
+
+    const people = await Person.findOne({ _id: id })
+
+    if (!people) {
+        res.status(422).json({ message: "Usuário não encontrado!" })
+        return
+    }
+
+    try {
+        await Person.deleteOne({ _id: id })
+        res.status(200).json({ message: "Usuário Removido com sucesso !" })
+    } catch (error) {
+        res.status(500).json({ erro: error })
+    }
+})
 
 mongoose.connect('mongodb://localhost:27017').then(() => {
     console.log('uhuul, conctamos')
